5e88e80bacfa0ed944958ae922936d3f99c2e6b1
alejandro montero <alejandromonteroart1551@gmail.com>
2023-05-31 22:41:31 -0400

    update

--- index.php
+++ index.php
@@ -6,6 +6,22 @@
 <body>
     <h1>Welcome to My Simple HTML Page</h1>
     <p>This is a basic HTML page displayed using PHP.</p>
-    <p>Current date and time: <?php echo date("Y-m-d H:i:s"); ?></p>
+
+    <?php
+    // Consume the API and display the data
+    $apiUrl = 'localhost/iot/api.php?endpoint=data';
+    $data = json_decode(file_get_contents($apiUrl), true);
+
+    if (!empty($data)) {
+        echo "<h2>Data from API:</h2>";
+        echo "<ul>";
+        foreach ($data as $row) {
+            echo "<li>" . $row['column_name'] . "</li>"; // Replace 'column_name' with the actual column name you want to display
+        }
+        echo "</ul>";
+    } else {
+        echo "No data available.";
+    }
+    ?>
 </body>
 </html>
