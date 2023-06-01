aec8da0e3cc97fe675c0dbb5efa34fe36f6d3ad8
alejandro montero <alejandromonteroart1551@gmail.com>
2023-05-31 22:33:39 -0400

    update

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
-<?php
-header("Content-Type: application/json");
-header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
-header("Access-Control-Allow-Origin: *");
-
-include_once 'db.php';
-include_once 'api.php';
-
-$db = new Database();
-$dbConn = $db->getConnection();
-
-$api = new Api($dbConn);
-
-$method = $_SERVER['REQUEST_METHOD'];
-
-switch($method) {
-  case 'GET':
-    $api->read();
-    break;
-  case 'POST':
-    $api->create();
-    break;
-  case 'PUT':
-    $api->update();
-    break;
-  case 'DELETE':
-    $api->delete();
-    break;
-  default:
-    http_response_code(405);
-    echo json_encode(array("message" => "This method is not allowed."));
-    break;
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple HTML Page</title>
+</head>
+<body>
+    <h1>Welcome to My Simple HTML Page</h1>
+    <p>This is a basic HTML page displayed using PHP.</p>
+    <p>Current date and time: <?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
